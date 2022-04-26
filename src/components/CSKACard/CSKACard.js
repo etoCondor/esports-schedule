9f7ba3a509fb4034c44ea1a6ccf8914a04689ab0
Alexey Kirsanov <etoCondor@yandex.ru>
2022-04-26 12:59:37 +0300

    date fix

--- src/components/CSKACard/CSKACard.js
+++ src/components/CSKACard/CSKACard.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { getCSKAMatches } from '../../fetchService/getMatches';
-import './style.css';
-import Spinner from '../spinner';
+import React, { useState, useEffect } from "react";
+import { getCSKAMatches } from "../../fetchService/getMatches";
+import "./style.css";
+import Spinner from "../spinner";
+import moment from "moment";
 
 function CSKACard() {
   const [loading, setLoading] = useState(true);
@@ -30,84 +31,6 @@ function CSKACard() {
     setLoading(false);
   }
 
-  function dateCorrector(date) {
-    const currentDate =
-      date.getDate().toString().length === 2
-        ? date.getDate()
-        : `0${date.getDate()}`;
-    const currentMonth =
-      (1 + date.getMonth()).toString().length === 2
-        ? date.getMonth() + 1
-        : `0${date.getMonth() + 1}`;
-    const currentHours =
-      date.getHours().toString().length === 2
-        ? date.getHours()
-        : `0${date.getHours()}`;
-    const currentMinutes =
-      date.getMinutes().toString().length === 2
-        ? date.getMinutes()
-        : `0${date.getMinutes()}`;
-    return `${currentDate}.${currentMonth}.${date.getFullYear()} 
-${currentHours}:${currentMinutes}`;
-  }
-
-  // function renderPlayers() {
-  //   if (!loading) {
-  //     let currentI = [];
-  //     // return teamMatches.data?.reverse().map((item, i) => {
-  //     return teamMatches.data?.map((item, i) => {
-  //       const date = new Date(item.match_start_iso);
-  //       let today = false;
-  //       if (
-  //         date.getDate() === new Date().getDate() &&
-  //         date.getMonth() === new Date().getMonth() &&
-  //         date.getYear() === new Date().getYear()
-  //       ) {
-  //         today = true;
-  //       }
-  //       if (
-  //         (item.home_team.team_id === 6184 ||
-  //           item.away_team.team_id === 6184) &&
-  //         currentI.length < 2 &&
-  //         date.getDate() >= new Date().getDate() &&
-  //         date.getMonth() >= new Date().getMonth() &&
-  //         date.getYear() >= new Date().getYear()
-  //       ) {
-  //         currentI.push(i);
-  //         return (
-  //           <div key={i} className={`footballCard ${today ? 'todayBall' : ''}`}>
-  //             <div className='versus'>
-  //               <img alt={item.home_team.name} src={item.home_team.logo}></img>
-
-  //               <div className='score'>
-  //                 <p>
-  //                   {item.home_team.name} vs {item.away_team.name}
-  //                 </p>
-
-  //                 <br />
-  //                 <p>
-  //                   {item.stats.home_score} : {item.stats.away_score}
-  //                 </p>
-  //                 <br />
-  //               </div>
-
-  //               <img alt={item.away_team.name} src={item.away_team.logo}></img>
-  //             </div>
-  //             <div className='date'>
-  //               <span>{dateCorrector(date)}</span>
-  //               <br />
-  //               {today ? <span>Today!</span> : null}
-  //             </div>
-  //           </div>
-  //         );
-  //       }
-  //       return null;
-  //     });
-  //   } else {
-  //     return null;
-  //   }
-  // }
-  // const items = renderPlayers(teamMatches);
   const currentI = [];
   return loading ? (
     <div className={`footballCard`}>
@@ -118,28 +41,23 @@ ${currentHours}:${currentMinutes}`;
       {teamMatches?.data?.map((item, i) => {
         const date = new Date(item.match_start_iso);
         let today = false;
-        if (
-          date.getDate() === new Date().getDate() &&
-          date.getMonth() === new Date().getMonth() &&
-          date.getYear() === new Date().getYear()
-        ) {
+
+        if (moment().isSame(date)) {
           today = true;
         }
         if (
           (item.home_team.team_id === 6184 ||
             item.away_team.team_id === 6184) &&
           currentI.length < 2 &&
-          date.getDate() >= new Date().getDate() &&
-          date.getMonth() >= new Date().getMonth() &&
-          date.getYear() >= new Date().getYear()
+          moment().isBefore(date)
         ) {
           currentI.push(i);
           return (
-            <div key={i} className={`footballCard ${today ? 'todayBall' : ''}`}>
-              <div className='versus'>
+            <div key={i} className={`footballCard ${today ? "todayBall" : ""}`}>
+              <div className="versus">
                 <img alt={item.home_team.name} src={item.home_team.logo}></img>
 
-                <div className='score'>
+                <div className="score">
                   <p>
                     {item.home_team.name} vs {item.away_team.name}
                   </p>
@@ -153,8 +71,8 @@ ${currentHours}:${currentMinutes}`;
 
                 <img alt={item.away_team.name} src={item.away_team.logo}></img>
               </div>
-              <div className='date'>
-                <span>{dateCorrector(date)}</span>
+              <div className="date">
+                <span>{moment(date).format("DD.MM.yyyy HH:mm")}</span>
                 <br />
                 {today ? <span>Today!</span> : null}
               </div>
--- src/components/esportCard/esportCard.js
+++ src/components/esportCard/esportCard.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { getMatches } from '../../fetchService/getMatches';
-import './style.css';
-import Spinner from '../spinner';
-import tbaImg from '../../img/tba.png';
-import tba2Img from '../../img/tba2.png';
+import React, { useState, useEffect } from "react";
+import { getMatches } from "../../fetchService/getMatches";
+import "./style.css";
+import Spinner from "../spinner";
+import tbaImg from "../../img/tba.png";
+import tba2Img from "../../img/tba2.png";
+import moment from "moment";
 
 function EsportCard({ teamID, teamName }) {
   const [loading, setLoading] = useState(true);
@@ -22,29 +23,8 @@ function EsportCard({ teamID, teamName }) {
     setLoading(false);
   }
 
-  function dateCorrector(date) {
-    const currentDate =
-      date.getDate().toString().length === 2
-        ? date.getDate()
-        : `0${date.getDate()}`;
-    const currentMonth =
-      (1 + date.getMonth()).toString().length === 2
-        ? date.getMonth() + 1
-        : `0${date.getMonth() + 1}`;
-    const currentHours =
-      date.getHours().toString().length === 2
-        ? date.getHours()
-        : `0${date.getHours()}`;
-    const currentMinutes =
-      date.getMinutes().toString().length === 2
-        ? date.getMinutes()
-        : `0${date.getMinutes()}`;
-    return `${currentDate}.${currentMonth}.${date.getFullYear()}
-${currentHours}:${currentMinutes}`;
-  }
-
   return loading ? (
-    <div className='footballCard nobg'>
+    <div className="footballCard nobg">
       <Spinner />
     </div>
   ) : (
@@ -53,47 +33,43 @@ ${currentHours}:${currentMinutes}`;
         const { scheduled_at } = item;
         const date = new Date(scheduled_at);
         let today = false;
-        if (
-          date.getDate() === new Date().getDate() &&
-          date.getMonth() === new Date().getMonth() &&
-          date.getYear() === new Date().getYear()
-        ) {
+        if (moment().isSame(date)) {
           today = true;
         }
         if (i < 1 || today) {
-          let winner = '';
-          item.winner_id === teamID ? (winner = 'green') : (winner = 'red');
+          let winner = "";
+          item.winner_id === teamID ? (winner = "green") : (winner = "red");
           if (item.winner_id === teamID) {
-            winner = 'green';
+            winner = "green";
           } else if (item.winner_id === null) {
-            winner = '';
+            winner = "";
           } else {
-            winner = 'red';
+            winner = "red";
           }
           return (
             <div
               key={i}
               className={`gameCard ${item.videogame.slug} ${
-                today ? 'today' : ''
+                today ? "today" : ""
               }`}
             >
-              <div className='videogame'>
+              <div className="videogame">
                 {teamName} {item.videogame.name}
               </div>
               <p>
-                {item.opponents[0]?.opponent.name || 'TBA'} vs{' '}
-                {item.opponents[1]?.opponent.name || 'TBA'}
+                {item.opponents[0]?.opponent.name || "TBA"} vs{" "}
+                {item.opponents[1]?.opponent.name || "TBA"}
               </p>
-              <div className='serie'>{item.serie.full_name}</div>
-              <div className='serie'>{item.name}</div>
-              <div className='serie'>Best of {item.number_of_games}</div>
-              <div className='versus'>
+              <div className="serie">{item.serie.full_name}</div>
+              <div className="serie">{item.name}</div>
+              <div className="serie">Best of {item.number_of_games}</div>
+              <div className="versus">
                 <img
                   alt={item.opponents[0]?.opponent.name}
                   src={item.opponents[0]?.opponent.image_url || tba2Img}
                 ></img>
 
-                <div className='score'>
+                <div className="score">
                   <p className={winner}>{`${item.results[0]?.score || 0} : ${
                     item.results[1]?.score || 0
                   }`}</p>
@@ -104,8 +80,8 @@ ${currentHours}:${currentMinutes}`;
                 ></img>
               </div>
 
-              <div className='date'>
-                <span>{dateCorrector(date)}</span>
+              <div className="date">
+                <span>{moment(date).format("DD.MM.yyyy HH:mm")}</span>
                 <br />
                 {today ? <span>Today!</span> : null}
               </div>
